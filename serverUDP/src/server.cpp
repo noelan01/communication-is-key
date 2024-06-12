b8115f079b223cc8bfe314e94375256ad7296ea6
noelan <n.andersson0110@gmail.com>
2024-06-12 22:24:16 +0200

    all good

--- serverUDP/src/server.cpp
+++ serverUDP/src/server.cpp
@@ -8,7 +8,6 @@ namespace udp
 {
 void server::init_server()
 {
-    std::cout << socket(AF_INET, SOCK_DGRAM, 0) << std::endl;
     // Creating socket file descriptor
     if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
         std::cout << "socket creation failed" << std::endl;
@@ -70,64 +69,6 @@ void server::recieve_data()
 
 int main()
 {
-    /*
-    int sockfd;
-    char buffer[BUFFER_SIZE];
-    struct sockaddr_in servaddr, cliaddr;
-
-    // Creating socket file descriptor
-    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-        std::cout << "socket creation failed" << std::endl;
-        perror("socket creation failed");
-        exit(EXIT_FAILURE);
-    }
-
-    memset(&servaddr, 0, sizeof(servaddr));
-    memset(&cliaddr, 0, sizeof(cliaddr));
-
-
-    // Filling server information
-    servaddr.sin_family = AF_INET; // IPv4
-    servaddr.sin_addr.s_addr = INADDR_ANY;
-    servaddr.sin_port = htons(PORT);
-
-    // Bind the socket with the server address
-    if (bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
-        std::cout << "bind failed" << std::endl;
-        perror("bind failed");
-        close(sockfd);
-        exit(EXIT_FAILURE);
-    }
-
-
-    while (true) {
-        socklen_t len = sizeof(cliaddr);
-        int n = recvfrom(
-            sockfd, (char *)buffer, BUFFER_SIZE, MSG_WAITALL,
-            (struct sockaddr *)&cliaddr, &len);
-        if (n < 0) {
-            perror("recvfrom error");
-            close(sockfd);
-            exit(EXIT_FAILURE);
-        }
-        buffer[n] = '\0';
-        std::cout << "Client: " << buffer << std::endl;
-
-        const char * hello = "Hello from server";
-        if (sendto(
-              sockfd, hello, strlen(
-                  hello), MSG_CONFIRM, (const struct sockaddr *)&cliaddr, len) < 0)
-        {
-            std::cout << "sendto error" << std::endl;
-            perror("sendto error");
-            close(sockfd);
-            exit(EXIT_FAILURE);
-        }
-        std::cout << "Hello message sent." << std::endl;
-    }
-    close(sockfd);
-    */
-
     udp::server server_1;
 
     server_1.init_server();
